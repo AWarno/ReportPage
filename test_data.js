25bdddf559b4f5388820aba3950aa65785b760dc
Anna Warno <anna.warno@7bulls.com>
2021-06-02 01:37:14 +0200

    descriptions added

--- test_data.js
+++ test_data.js
@@ -1,13 +1,19 @@
 var data = {
     "Kompetencjometr": {
-        "free": "1", "time": "20", "lang": "pl", "cat": "1", "img": "images/2.png", "link": "http://kompetencjometr.mlodziwlodzi.pl/"
+        "free": "1", "time": "20", "lang": "pl", "cat": "1", "img": "images/2.png", "link": "http://kompetencjometr.mlodziwlodzi.pl/", "desc": "Test stylu rozwiązywania konfliktów"
     },
-    "Co po maturze?": { "free": "1", "time": "10", "lang": "en", "cat": "0", "img": "images/3.png", "link": "https://copomaturze.lazarski.pl/ " },
-    "16 personalities": { "free": "1", "time": "15", "lang": "pl", "cat": "0", "img": "images/4.png", "link": "https://www.16personalities.com/pl " },
-    "Predyspozycje zawodowe": { "free": "0", "time": "45", "lang": "en", "cat": "0", "img": "images/5.png", "link": "https://predyspozycje-zawodowe.pl/ " },
-    "WOPZ": { "free": "1", "time": "30", "lang": "pl", "cat": "1", "img": "images/6.png", "link": "https://wybierz-zawod.eu/" },
-    "Test Kariery": { "free": "0", "time": "12", "lang": "en", "cat": "1", "img": "images/7.png", "link": "http://testkariery.pl/" },
-    "Test Hartmana": { "free": "0", "time": "15", "lang": "en", "cat": "1", "img": "images/8.png", "link": "https://testhartmana.pl/ " },
+    "Co po maturze?": { "free": "1", "time": "10", "lang": "en", "cat": "0", "img": "images/3.png", "link": "https://copomaturze.lazarski.pl/ ", "desc": "Test stylu rozwiązywania konfliktów" },
+    "16 personalities": { "free": "1", "time": "15", "lang": "pl", "cat": "0", "img": "images/4.png", "link": "https://www.16personalities.com/pl ", "desc": "Test stylu rozwiązywania konfliktów" },
+    "Predyspozycje zawodowe": { "free": "0", "time": "45", "lang": "en", "cat": "0", "img": "images/5.png", "link": "https://predyspozycje-zawodowe.pl/ ", "desc": "Test stylu rozwiązywania konfliktów" },
+    "WOPZ": { "free": "1", "time": "15", "lang": "pl", "cat": "1", "img": "images/6.png", "link": "https://wybierz-zawod.eu/", "desc": "przeznaczone do diagnozy preferencji zawodowych", "page": "tests/5.html" },
+    "Test Kariery": { "free": "1", "time": "10", "lang": "pl", "cat": "1", "img": "images/7.png", "link": "http://testkariery.pl/", "desc": "Test osobowości (Meyersa-Briggsa) ", "page": "tests/6.html" },
+    "Test Hartmana": { "free": "1", "time": "10", "lang": "pl", "cat": "0", "img": "images/8.png", "link": "https://testhartmana.pl/ ", "desc": "Test osobowości (dr Hartman)", "page": "tests/7.html" },
+    "Test Thomasa Kilmanna": { "free": "1", "time": "-", "lang": "pl", "cat": "0", "img": "images/9.png", "link": "https://www.cdr.gov.pl/pol/o_poznan/metodyka/rozw_zadan_8_3.pdf", "desc": "Test stylu rozwiązywania konfliktów", "page": "tests/8.html" },
+    "Test Belbina": { "free": "0", "time": "-", "lang": "pl", "cat": "0", "img": "images/9.png", "link": "https://www.belbin.pl/", "desc": "Dostępny w dwóch wersjach. (w tym płatnej)", "page": "tests/9.html" },
+    "Test Gallupa": { "free": "0", "time": "60", "lang": "en", "cat": "1", "img": "images/9.png", "link": "https://www.gallup.com/cliftonstrengths/en/home.aspx", "desc": "Pomaga zidentyfikowac talenty", "page": "tests/10.html" },
+    "Zamek": {
+        "free": "1", "time": "25", "lang": "pl", "cat": "1", "img": "images/9.png", "link": "http://www.e-zamek.pl/  ", "desc": "Test predyspozycji zawodowych", "page": "tests / 11.html"
+    },
 };
 
 
@@ -40,23 +46,21 @@ function createCard(test_id) {
     // start_card = start_card + test_id + '</h2>';
     start_card += '<a class="test_link" href=' + String(data[test_id]["link"]) + '>' + test_id + '</a>';
     // start_card = '<a href="' + String(data[test_id]["link"]) + '">' + test_id + '</a>';
-    start_card = start_card + `
-            <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout`;
+    start_card = start_card + '<p class="card_text">' + data[test_id]["desc"];
 
-
-    start_card = start_card + data[test_id]["free"] + data[test_id]["cat"] + data[test_id]["lang"] + '</p>';
     start_card = start_card + '<div style="text-align: center; font-weight: 900; display: grid; grid-template-columns: auto auto auto;"><p>jęz: ';
     start_card = start_card + data[test_id]["lang"] + '</p>';
     // start_card = start_card + '<img src=' + '"pl.png"' + '>';
     start_card = start_card + '<p>czas: ' + data[test_id]["time"] + 'min</p>';
 
-    if (data[test_id]["free"] == "1") {
+    if (data[test_id]["free"] == "0") {
         start_card = start_card + '<p> $$$ </p>';
 
     }
 
     start_card = start_card + '</div>';
-    start_card = start_card + `<a class="btn" href="./tests/test_desc.html">Więcej</a>
+    start_card = start_card + '<a class="btn" href="' + data[test_id]["page"] + '">Więcej</a>';
+    start_card = start_card + `
         </div>
         </div><i class="icofont-clock-time"></i>
     </li><i class="icofont-clock-time"></i>
